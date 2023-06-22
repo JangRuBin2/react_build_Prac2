eb8f0d4dfad91e4c383423143a5292c4a1547d31
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-06-22 22:21:54 +0900

    entry 파일 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+// App 컴포넌트 작성
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+};
+
+// App 컴포넌트를 실제 DOM에 렌더링
+ReactDOM.render(<App />, document.getElementById('root'));
